2f634c46a0b8c92987a8930d168ecf4a7fd75c6e
khalilpreview <previewsites40@gmail.com>
2020-05-10 01:29:25 +0100

    version beta

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField , SelectField ,SubmitField 
 from wtforms.validators import InputRequired
 import pyowm
+import time 
 
 # Flask app initialisation
 app = Flask(__name__)
@@ -14,20 +15,8 @@ app.config.from_pyfile('config.cfg')
 api_key = "f8b0c8978211db711199ec4a17834237"
 owm_obj = pyowm.OWM(api_key)
 
-
-# Search form by Flask-wtf
-class SearchBarre(FlaskForm):
-    sbarre = StringField(validators=[InputRequired()])
-    country_code = SelectField(u'Country code', choices=[] , validators=[InputRequired()])
-    submit = SubmitField('Search')
-
-# Home route /
-@app.route('/' , methods=['GET','POST'])
-def home():
-    form = SearchBarre() # our Form 
-    index_page = True
-    weather_info = {} # 
-    countryCode = ['af', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'ar',
+# all world wide country code
+countryCode = ['af', 'al', 'dz', 'as', 'ad', 'ao', 'ai', 'aq', 'ag', 'ar',
      'am', 'aw', 'au', 'at', 'az', 'bs', 'bh', 'bd', 'bb', 'by', 'be', 'bz', 
      'bj', 'bm', 'bt', 'bo', 'ba', 'bw', 'bv', 'br', 'io', 'bn', 'bg', 'bf', 
      'bi', 'kh', 'cm', 'ca', 'cv', 'ky', 'cf', 'td', 'cl', 'cn', 'cx', 'cc', 'co',
@@ -47,6 +36,20 @@ def home():
         'ug', 'ua', 'ae', 'gb', 'us', 'um', 'uy', 'uz', 'vu', 'va', 've', 'vn', 'vg',
          'vi', 'wf', 'eh', 'ye', 'yu', 'zr', 'zm', 'zw']
 
+
+# Search form by Flask-wtf
+class SearchBarre(FlaskForm):
+    sbarre = StringField(validators=[InputRequired()])
+    country_code = SelectField(u'Country code', choices=[] , validators=[InputRequired()])
+    submit = SubmitField('Search')
+
+# Home route /
+@app.route('/' , methods=['GET','POST'])
+def home():
+    form = SearchBarre() # our Form 
+    weather_info = {} # empty dictionary for weather info
+    index_page = True
+    
     form.country_code.choices = [(code.upper() , code.upper())for code in countryCode ]
 
     if form.validate_on_submit():
@@ -55,15 +58,18 @@ def home():
         weather = obs_obj.get_weather() # find weather for user input
         city = str(form.sbarre.data) # city name from user input
         index_page = False 
+        now = time.strftime('%c')
 
         weather_info = {
+            'timing':str(now),
             'statu' : str(weather.get_status()),
             'detailed-statu': str(weather.get_detailed_status()),
             'temperature':str(weather.get_temperature(unit='celsius')['temp']) + '°',
             'temp-max':str(weather.get_temperature(unit='celsius')['temp_max']) + '°',
             'temp-min':str(weather.get_temperature(unit='celsius')['temp_min']) + '°',
             'humidity': str(weather.get_humidity()) + ' %',
-            'wind': str(weather.get_wind()['speed']) + ' m/s ' + str(weather.get_wind()['deg']) + '°',
+            'pressure': str(weather.get_pressure()['press']) + ' hpa',
+            'wind': str(weather.get_wind()['speed']) + ' m/s ',
             'sun-rise': weather.get_sunrise_time(timeformat='iso').split(),
             'sun-set': weather.get_sunset_time(timeformat='iso').split(),
         }
